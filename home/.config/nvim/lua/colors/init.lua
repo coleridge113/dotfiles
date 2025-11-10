363cca861800ec6a48fd99db5a9a466eb8b8d86f
Jose Luna <josemanuelmluna@gmail.com>
2025-11-10 15:57:02 +0800

    removed nvim as submodule

---
+++ home/.config/nvim/lua/colors/init.lua
@@ -0,0 +1,12 @@
+local active = "github"
+
+local themes = {
+    ["rose-pine"] = require("colors.themes.rose-pine"),
+    ["tokyonight"] = require("colors.themes.tokyonight"),
+    ["gruvbox"] = require("colors.themes.gruvbox"),
+    ["github"] = require("colors.themes.github")
+}
+
+return {
+    themes[active],
+}
---
+++ home/.config/nvim/lua/config/lazy.lua
@@ -0,0 +1,83 @@
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+-- Line Numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- Tab Width
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.expandtab = true
+
+-- Default to clipboard
+vim.opt.clipboard = "unnamedplus"
+
+-- Terminal shortcuts
+vim.keymap.set("n", "<leader>tt", ":terminal<CR>", { desc = "Open terminal" })
+vim.keymap.set("n", "<leader>tv", ":rightbelow vsp|terminal<CR>", { desc = "Vertical split terminal" })
+vim.keymap.set("n", "<leader>ts", ":belowright sp|terminal<CR>", {desc = "Horizontal split terminal" })
+vim.keymap.set("n", "<leader>w", "<C-w>", { desc = "Initiate switch window" } )
+
+-- Translate to unix when copy-pasting
+vim.api.nvim_create_autocmd({"BufRead"}, {
+  pattern = "*",
+  callback = function(args)
+    vim.bo[args.buf].fileformat = "unix"
+    -- Remove carriage returns in-place
+    vim.cmd([[%s/\r//ge]])
+  end,
+})
+
+-- Map <Esc> in terminal mode
+vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", { noremap = true, silent = true })
+
+-- Ignore case
+vim.opt.ignorecase = true
+vim.keymap.set("n", "<leader>ic", ":set ignorecase!<CR>", { desc = "Toggle ignore case" })
+
+-- No highlight search
+vim.keymap.set("n", "<Esc>", ":nohlsearch<CR>", { silent = true })
+
+-- Remap quit
+vim.keymap.set("n", "<leader>q", "<cmd>q<CR>", { desc = "Close page" })
+vim.keymap.set("n", "<leader>Q", "<cmd>qall<CR>", { desc = "Close all pages" })
+
+-- Setup lazy.nvim
+require("lazy").setup({
+  spec = {
+    -- import your plugins
+    { import = "plugins" },
+    { import = "colors" },
+  },
+  -- Configure any other settings here. See the documentation for more details.
+  -- colorscheme that will be used when installing plugins.
+  install = { colorscheme = { "habamax" } },
+  -- automatically check for plugin updates
+  checker = { enabled = true },
+  rocks = { 
+      enabled = false,
+      hererocks = false,
+  },
+})
