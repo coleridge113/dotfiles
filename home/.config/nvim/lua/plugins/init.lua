700a417bc5a5247a246c6828ce0df10af0c52468
Jose Luna <josemanuelmluna@gmail.com>
2025-11-12 17:49:00 +0800

    added nvim-surround

--- home/.config/nvim/lua/plugins/init.lua
+++ home/.config/nvim/lua/plugins/init.lua
@@ -92,7 +92,7 @@ return {
             require("mason-lspconfig").setup({ ensure_installed = servers })
 
             local function is_lsp_attached(server_name, root_dir)
-                local clients = vim.lsp.get_active_clients({ bufnr = 0 })
+                local clients = vim.lsp.get_clients({ bufnr = 0 })
                 for _, client in ipairs(clients) do
                     if client.name == server_name and client.root_dir == root_dir then
                         return true
@@ -273,4 +273,14 @@ return {
             vim.api.nvim_create_autocmd({ 'BufWinEnter' }, { callback = function() require('mini.map').open() end })
         end,
     },
+    {
+        "kylechui/nvim-surround",
+        version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
+        event = "VeryLazy",
+        config = function()
+            require("nvim-surround").setup({
+                -- Configuration here, or leave empty to use defaults
+            })
+        end
+    }
 }
