953c6a9c92825f99d613e061260485b146090c8f
Jose Luna <josemanuelmluna@gmail.com>
2025-12-29 18:21:12 +0800

    added aliases and fixed treesitter

--- home/.config/nvim/lua/plugins/init.lua
+++ home/.config/nvim/lua/plugins/init.lua
@@ -4,7 +4,7 @@ return {
         "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
         config = function()
-            require("nvim-treesitter.configs").setup({
+            require("nvim-treesitter.config").setup({
                 ensure_installed = {
                     "lua", "vim", "vimdoc", "query",
                     "kotlin", "bash", "json", "yaml",
