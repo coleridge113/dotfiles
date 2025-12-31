dc09caa83df0f185d22f09133bde21f97928f0b3
Jose Luna <josemanuelmluna@gmail.com>
2025-12-31 14:05:48 +0800

    added light themed gruvbox

--- home/.config/nvim/lua/colors/init.lua
+++ home/.config/nvim/lua/colors/init.lua
@@ -1,9 +1,10 @@
-local active = "gruvbox"
+local active = "gruvbox-light"
 
 local themes = {
     ["rose-pine"] = require("colors.themes.rose-pine"),
     ["tokyonight"] = require("colors.themes.tokyonight"),
     ["gruvbox"] = require("colors.themes.gruvbox"),
+    ["gruvbox-light"] = require("colors.themes.gruvbox-light"),
     ["github"] = require("colors.themes.github")
 }
 
