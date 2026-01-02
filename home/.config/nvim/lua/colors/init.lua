b5538901e598b19d8db61f1630742eff5959be4e
Jose Luna <josemanuelmluna@gmail.com>
2026-01-02 21:16:22 +0800

    added gruvbox material

--- home/.config/nvim/lua/colors/init.lua
+++ home/.config/nvim/lua/colors/init.lua
@@ -1,9 +1,10 @@
-local active = "everforest"
+local active = "tokyonight"
 
 local themes = {
     ["rose-pine"] = require("colors.themes.rose-pine"),
     ["tokyonight"] = require("colors.themes.tokyonight"),
     ["gruvbox"] = require("colors.themes.gruvbox"),
+    ["gruvbox-material"] = require("colors.themes.gruvbox-material"),
     ["github"] = require("colors.themes.github"),
     ["kanagawa"] = require("colors.themes.kanagawa"),
     ["sonokai"] = require("colors.themes.sonokai"),
