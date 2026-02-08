939b8523e0e8200c98f66e8dffb99ca00e13ce72
Jose Luna <josemanuelmluna@gmail.com>
2026-02-08 10:51:23 +0800

    added leetcode plugin

--- home/.config/nvim/lua/plugins/init.lua
+++ home/.config/nvim/lua/plugins/init.lua
@@ -376,5 +376,18 @@ return {
         keys = {
             { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
         }
+    },
+    -- Leet code
+    {
+        "kawre/leetcode.nvim",
+        build = ":TSUpdate html", -- if you have `nvim-treesitter` installed
+        dependencies = {
+            -- include a picker of your choice, see picker section for more details
+            "nvim-lua/plenary.nvim",
+            "MunifTanjim/nui.nvim",
+        },
+        opts = {
+            -- configuration goes here
+        },
     }
 }
