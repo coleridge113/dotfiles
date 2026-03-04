0acac33e748f4441fcb85deb6580b76f8dfdd45d
Jose Luna <josemanuelmluna@gmail.com>
2026-03-04 11:17:55 +0800

    modified gruvbox material

--- home/.config/nvim/lua/colors/themes/gruvbox-material.lua
+++ home/.config/nvim/lua/colors/themes/gruvbox-material.lua
@@ -1,11 +1,21 @@
 return {
-    'sainnhe/gruvbox-material',
+    "sainnhe/gruvbox-material",
     lazy = false,
     priority = 1000,
     config = function()
-        -- Optionally configure and load the colorscheme
-        -- directly inside the plugin declaration.
         vim.g.gruvbox_material_enable_italic = true
-        vim.cmd.colorscheme('gruvbox-material')
-    end
+
+        -- load theme
+        vim.cmd.colorscheme("gruvbox-material")
+
+        -- override highlights AFTER colorscheme loads
+        vim.api.nvim_set_hl(0, "Visual", {
+            bg = "#cab692",
+            fg = "#282828",
+        })
+
+        vim.api.nvim_set_hl(0, "CursorLine", {
+            bg = "#32302f",
+        })
+    end,
 }
