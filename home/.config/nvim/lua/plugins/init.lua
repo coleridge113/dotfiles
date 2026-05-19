293a5e7804d0506665a71fb8279ab628796f9ccb
Jose Luna <josemanuelmluna@gmail.com>
2026-05-19 12:43:53 +0800

    added mini.ai

--- home/.config/nvim/lua/plugins/init.lua
+++ home/.config/nvim/lua/plugins/init.lua
@@ -514,5 +514,44 @@ return {
                 typescriptreact = { "prettier" },
             },
         }
-    }
+    },
+    -- Mini AI
+    {
+        'echasnovski/mini.ai',
+        version = false,
+        config = function()
+            require('mini.ai').setup({
+                -- Table with textobject id as fields, textobject specification as values.
+                -- Also use this to disable builtin textobjects. See |MiniAi.config|.
+                custom_textobjects = nil,
+
+                -- Module mappings. Use `''` (empty string) to disable one.
+                mappings = {
+                    -- Main textobject prefixes
+                    around = 'a',
+                    inside = 'i',
+
+                    -- Next/last variants
+                    -- NOTE: This overrides Neovim>=0.12 built-in incremental selection mappings.
+                    around_next = 'an',
+                    inside_next = 'in',
+                    around_last = 'al',
+                    inside_last = 'il',
+
+                    -- Move cursor to corresponding edge of `a` textobject
+                    goto_left = 'g[',
+                    goto_right = 'g]',
+                },
+
+                -- Number of lines within which textobject is searched
+                n_lines = 50,
+
+                -- How to search for object
+                search_method = 'cover_or_next',
+
+                -- Whether to disable showing non-error feedback
+                silent = false,
+            })
+        end,
+    },
 }
